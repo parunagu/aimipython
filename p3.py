d486a9fb4b4061d5d1b4f9081ec21d336fcbc644
paru <paru@gmail.com>
2023-08-14 17:19:15 +0530

    changes3

---
+++ p3.py
@@ -0,0 +1,13 @@
+a=[1,2,3,4]
+b=[3,5,7,8]
+c=[6,8,3,5]
+l=list(map(lambda a,b,c:a+b+c, a,b,c))
+print(l)
+
+lst=["mango","apple","orange","watermelon"]
+l=list(filter(lambda x:"g" in x,lst))
+print(l)
+ 
+lst=["cat","man","table","pen","book","racchu","acchu","nagu"]
+l1=list(filter(lambda x:"e" in x,lst))
+print(l1)
