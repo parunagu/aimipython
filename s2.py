d61aea93b7cbd53c82e579fa584199a8db18b3de
paru <paru@gmail.com>
2023-08-14 21:28:43 +0530

    change2

---
+++ s2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+ser1=pd.Series([10,12,11,11])
+ser2=pd.Series([1,12,11,3,5])
+ 
+u=pd.Series(np.union1d(ser1,ser2))
+print("_______union_______",u)
+
+i=pd.Series(np.intersect1d(ser1,ser2))
+print("______intersect_______",i)
+
+nc=u[~ u.isin(i)]
+print("_____remove common number_____",nc) 
